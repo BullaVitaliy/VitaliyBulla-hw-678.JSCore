d8a5a3c21944b0773a7ae38f6a359b40156f8304
Vitaliy <bullavitaliy@gmil.com>
2022-03-23 09:47:38 +0200

    hw3 to3

---
+++ homeWork3/homeWork3.js
@@ -0,0 +1,47 @@
+//task-1
+// const array = [2, 3, 4, 5];
+
+//for
+// let result = 1;
+// for (let i = 0; i < array.length; i++) {
+//     result = result * array[i]
+// }
+// console.log(result);
+
+//while
+// let result = 1;
+// let i = 0;
+// while(i < array.length) {
+//     result = result * array[i]
+//     i++
+// }
+// console.log(result);
+
+
+//task-2
+// for(let i = 0; i <= 15; i++) {
+//     i % 2 == 0 ? document.write(`${i} is even <br \/>`) : document.write(`${i} is odd <br \/>`) ;
+
+//     // if(i % 2 == 0) {
+//     //     document.write(`${i} is even`)
+//     // } else {
+//     //     document.write(`${i} is odd`)
+//     // }
+// }
+
+
+//task-3
+// const array = [];
+
+// function randArray(count) {
+//     for(let i = 1; i <= count; i++) { 
+//         array.push(Math.floor(Math.random() * 500) + 1)
+//     }  
+// }
+
+// randArray(5)
+// console.log(array);
+
+
+//task-4
+
