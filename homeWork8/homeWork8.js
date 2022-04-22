e17a5f88978972f04d18b9f1bd914daa0ba677dc
Vitaliy <bullavitaliy@gmil.com>
2022-04-23 02:46:23 +0300

    hw8

--- homeWork8/homeWork8.js
+++ homeWork8/homeWork8.js
@@ -3,7 +3,7 @@ function upperCase(str) {
   let regExp = /^[A-Z]/;
   let result = regExp.test(str);
   if (result === true) {
-    console.log("Strings starts with uppercase character");
+    console.log("String's starts with uppercase character");
   } else if (result === false) {
     console.log("String's not starts with uppercase character");
   }
